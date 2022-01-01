7894d46b43d87d1d3aa780b9a407e4ed6bad63ea
csegura <romheat@gmail.com>
2022-01-01 22:25:52 +0100

    clean

--- b-client/b-client.c
+++ b-client/b-client.c
@@ -11,48 +11,16 @@
 #include <pthread.h>
 #include "../b-server/b-funcs.h"
 
-#define COMMAND_LEN 128
-#define BUFFER_LEN 4096
-#define MAX_CONNECTIONS 1000
-
-char *get_command()
-{
-	char *cmd = calloc(COMMAND_LEN, sizeof(char));
-	int n = 0;
-	printf("\nCMD: ");
-	while ((cmd[n++] = getchar()) != '\n')
-		;
-	return cmd;
-}
-
-// void check_message(message_t *message)
-// {
-// 	hash_value received_hash = get_hash(message);
-// 	hash_value calculated_hash = calc_hash(message, 1);
-
-// 	if (received_hash != calculated_hash)
-// 	{
-// 		printf("ERROR: hash mismatch - Received %d Calc %d\n",
-// 					 received_hash,
-// 					 calculated_hash);
-// 	}
-// 	else
-// 	{
-// 		message->hash.value = received_hash;
-// 		// todo: adjust message->size
-// 	}
-// }
+#define MAX_CONNECTIONS 50
 
 message_t *get_message(request_t *request)
 {
 
 	size_t size;
 	read(request->fd, &size, sizeof(size_t));
-	printf("size: %d\n", size);
 
 	hash_t hash;
 	read(request->fd, &hash, sizeof(hash_value));
-	printf("hash: %d\n", hash);
 
 	message_t *message = create_message(NULL, 0);
 	size_t buffer_size = size > 65535 ? 65535 : size;
@@ -68,18 +36,14 @@ message_t *get_message(request_t *request)
 		message->size += received;
 		if (size < buffer_size)
 			break;
-		bzero(chunk, buffer_size);
+		//bzero(chunk, buffer_size);
 	}
 
-	printf("RCVD bytes: %d - %d\n", message->size, size);
 	message->hash = calc_hash(message);
 
 	if (hash != message->hash)
 	{
-
-		printf("ERROR: hash mismatch - Received %d Calc %d\n",
-					 hash,
-					 message->hash);
+		message->hash = 0;
 	}
 
 	return message;
@@ -88,14 +52,9 @@ message_t *get_message(request_t *request)
 void *client(void *args)
 {
 	request_t *request = (request_t *)args;
-
 	message_t *message;
 	time_t start, end;
-
-	// printf("[%2d:%3d] SENT: %s cmd\n",
-	// 			 request->fd,
-	// 			 request->serial,
-	// 			 bytes_to_human(request->size));
+	int *error = malloc(sizeof(int));
 
 	request_message_t *request_message = create_request_message(request);
 	write(request->fd, request_message, sizeof(request_message_t));
@@ -111,10 +70,14 @@ void *client(void *args)
 				 bytes_to_human(message->size),
 				 (double)(end - start) / CLOCKS_PER_SEC / 1000);
 
+	*error = message->hash == 0 ? 1 : 0;
+
 	delete_message(message);
 	close(request->fd);
 	delete_request(request);
 	delete_request_message(request_message);
+
+	pthread_exit(error);
 }
 
 int connect_server(char *server, int port)
@@ -123,7 +86,7 @@ int connect_server(char *server, int port)
 	int sockfd, connfd;
 	struct sockaddr_in servaddr, cli;
 
-	printf("Connecting to %s:%d\n", server, port);
+	//printf("Connecting to %s:%d\n", server, port);
 
 	// socket create and varification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -133,8 +96,6 @@ int connect_server(char *server, int port)
 		printf("Socket creation failed...\n");
 		exit(0);
 	}
-	//else
-	//printf("Socket successfully created..\n");
 
 	bzero(&servaddr, sizeof(servaddr));
 
@@ -151,10 +112,23 @@ int connect_server(char *server, int port)
 		printf("Connection failed.\n");
 		exit(0);
 	}
-	else
-		printf("Connected.\n");
 
 	return sockfd;
+}
+
+int wait_threads(pthread_t *threads, int n_threads)
+{
+	int errors = 0;
+	int *error;
+	int i;
+
+	for (i = 0; i < n_threads; i++)
+	{
+		pthread_join(threads[i], (void *)&error);
+		errors += *error;
+		free(error);
+	}
+	return errors;
 }
 
 int main(int argc, char **argv)
@@ -162,10 +136,11 @@ int main(int argc, char **argv)
 	int opt;
 	int port = DEFAULT_PORT;
 	char *server = DEFAULT_SERVER;
-	int threads = 1;
+	int n_requests = 0;
 	char *command = NULL;
-	pthread_t thread[MAX_CONNECTIONS];
-	int connections = 0;
+	pthread_t threads[MAX_CONNECTIONS];
+	int n_threads = 0;
+	time_t start, end;
 
 	while ((opt = getopt(argc, argv, "s:p:f:c:")) != -1)
 	{
@@ -178,7 +153,7 @@ int main(int argc, char **argv)
 			port = atoi(optarg);
 			break;
 		case 'f':
-			threads = atoi(optarg);
+			n_requests = atoi(optarg);
 			break;
 		case 'c':
 			command = optarg;
@@ -194,46 +169,62 @@ int main(int argc, char **argv)
 
 	if (!server)
 	{
-		printf("Usage: %s -s <server> [-p <port>]\n", argv[0]);
+		printf("Usage: %s -s <server> [-p <port>] -c <size_in_kb> -f <n_requests>\n", argv[0]);
 		exit(0);
 	}
 
-	printf("\ncommand: %s | proccess %d\n", command, threads);
+	printf("n_requests: %d\n", n_requests);
 
-	for (int i = 0; i < threads; i++)
+	int errors = 0;
+	size_t size = atoi(command) * 1024;
+	time(&start);
+
+	for (int i = 0; i < n_requests; i++)
 	{
 		int connfd = connect_server(server, port);
 
-		size_t size = atoi(command) * 1024;
-
-		printf("%lu %s\n", size, bytes_to_human(size));
-
 		request_t *request = create_request(connfd, i, size, 0);
 
-		if (pthread_create(&thread[connections++], NULL, client, (void *)request) < 0)
+		if (pthread_create(&threads[n_threads++], NULL, client, (void *)request) < 0)
 		{
 			perror("could not create thread");
 			exit(1);
 		}
 
-		if (connections >= MAX_CONNECTIONS)
+		if (n_threads > MAX_CONNECTIONS)
 		{
-			printf("Maximum connections reached. (%d)\n", connections);
+			printf("Maximum connections reached. (%d)\n", MAX_CONNECTIONS);
 
-			for (connections = 0; connections < MAX_CONNECTIONS; connections++)
-			{
-				pthread_join(thread[connections], NULL);
-			}
+			// for (connections = 0; connections < MAX_CONNECTIONS; connections++)
+			// {
+			// 	int *error = NULL;
+			// 	pthread_join(thread[connections], (void *)&error);
+			// 	errors += *error;
+			// 	free(error);
+			// }
 
-			printf("Finished. (%d)\n", connections);
-
-			connections = 0;
+			errors += wait_threads(threads, MAX_CONNECTIONS);
+			n_threads = 0;
 		}
 	}
 
-	// wait for all threads to finish
-	for (int i = 0; i < threads; i++)
-		pthread_join(thread[i], NULL);
+	errors += wait_threads(threads, MAX_CONNECTIONS);
+
+	// // wait for all threads to finish
+	// for (int i = 0; i < MAX_CONNECTIONS; i++)
+	// {
+	// 	int *error;
+	// 	pthread_join(thread[i], (void *)&error);
+	// 	errors += (int)*error;
+	// 	free(error);
+	// }
+
+	time(&end);
+
+	printf("Elapsed time %2fs - %d errors - %s reived\n",
+				 (double)(end - start),
+				 errors,
+				 bytes_to_human(size * n_requests));
 
 	return 0;
 }
